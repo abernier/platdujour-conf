3302a1b78499c30d916d3087cdea59a0510707ff
Antoine BERNIER <antoine.bernier@gmail.com>
2013-05-23 15:54:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var rc = require('rc');
 
 module.exports = rc('platdujour', {
-  couch_host: 'http://localhost:5984' // default CouchDB in admin party
+  couch_host: 'http://localhost:5984', // default CouchDB in admin party mode
+  couch_db: '/platdujour'
 });
