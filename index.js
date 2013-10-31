d71d76289d236a99d3236d76ca2b54ce977b5bd4
Antoine BERNIER <antoine.bernier@gmail.com>
2013-10-31 17:13:55 +0100

    typo2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var ini = require('ini');
 
-var defaults = ini.parse(fs.readFileSync('./defaults.ini', 'utf-8'));
+var defaults = ini.parse(fs.readFileSync(__dirname + '/defaults.ini', 'utf-8'));
 
 var rc = require('rc');
 
